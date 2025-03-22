4698ceebba9b7786e0f86fc15a4225b5a3c66dd8
herotux <thefreetux@gmail.com>
2025-03-22 15:12:41 +0330

    fix bug

--- app/database.py
+++ app/database.py
@@ -8,3 +8,13 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+
+# تابع get_db
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
